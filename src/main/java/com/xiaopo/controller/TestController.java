453f88a1e210252d7f5c1bd5982005d339aca3f9
jxp <jxp@qq.com>
2020-08-23 10:43:21 +0800

    w

--- src/main/java/com/xiaopo/controller/TestController.java
+++ src/main/java/com/xiaopo/controller/TestController.java
@@ -4,7 +4,5 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("213");
         System.out.println("213");
-        System.out.println("222");
-        System.out.println("222");
     }
 }
