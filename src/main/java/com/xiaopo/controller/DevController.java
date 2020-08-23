551a6a4fae4909d20507aada495cde4bd09751f8
jxp <jxp@qq.com>
2020-08-23 08:59:26 +0800

    dev-01 01

--- src/main/java/com/xiaopo/controller/DevController.java
+++ src/main/java/com/xiaopo/controller/DevController.java
@@ -3,5 +3,6 @@ package com.xiaopo.controller;
 public class DevController {
     public static void main(String[] args) {
         System.out.println("dev");
+        System.out.println("01");
     }
 }
