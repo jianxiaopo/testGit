16e25bfe7ff916d7e2e9f32257b1517bdc4934f6
jxp <jxp@qq.com>
2020-08-23 08:35:15 +0800

    add hotfix-2 01

--- src/main/java/com/xiaopo/controller/DevController.java
+++ src/main/java/com/xiaopo/controller/DevController.java
@@ -3,5 +3,6 @@ package com.xiaopo.controller;
 public class DevController {
     public static void main(String[] args) {
         System.out.println("dev");
+        System.out.println("hotfix-2 01");
     }
 }
