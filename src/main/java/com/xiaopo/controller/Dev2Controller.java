dbbb525a86f7e003162874c15b6e79518dfad6e3
jxp <jxp@qq.com>
2020-08-23 08:53:15 +0800

    dev-02

--- src/main/java/com/xiaopo/controller/Dev2Controller.java
+++ src/main/java/com/xiaopo/controller/Dev2Controller.java
@@ -3,5 +3,6 @@ package com.xiaopo.controller;
 public class Dev2Controller {
     public static void main(String[] args) {
         System.out.println("dev2");
+        System.out.println("new dev-02");
     }
 }
