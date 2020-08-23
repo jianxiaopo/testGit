af45861e7be2e2c86a795741dfa86e68244c1726
jxp <jxp@qq.com>
2020-08-23 08:49:18 +0800

    delete

--- src/main/java/com/xiaopo/controller/DevController.java
+++ src/main/java/com/xiaopo/controller/DevController.java
@@ -3,7 +3,5 @@ package com.xiaopo.controller;
 public class DevController {
     public static void main(String[] args) {
         System.out.println("dev");
-        System.out.println("hot-fix-3");
-        System.out.println("hotfix-2 01");
     }
 }
