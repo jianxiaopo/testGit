67ef826854bf3c0659eeab58f3ca89f57de56ca9
jxp <jxp@qq.com>
2020-08-23 09:06:17 +0800

    dev2

--- src/main/java/com/xiaopo/controller/Test2Controller.java
+++ src/main/java/com/xiaopo/controller/Test2Controller.java
@@ -4,5 +4,6 @@ public class Test2Controller {
     public static void main(String[] args) {
         System.out.println("test2");
         System.out.println("new dev-02");
+        System.out.println("ww");
     }
 }
